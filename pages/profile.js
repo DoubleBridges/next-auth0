a4313ab47a22894fe1f39fa7ead6b327c2253e45
Dave Kane <davelkane@gmail.com>
2020-04-16 17:20:49 -0500

    Added env var's

--- pages/profile.js
+++ pages/profile.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
 // This import is only needed when checking authentication status directly from getInitialProps
 // import auth0 from '../lib/auth0'
-import { useFetchUser } from '../lib/user'
-import Layout from '../components/layout'
-import withApollo from '../lib/apollo'
+import { useFetchUser } from "../lib/user";
+import Layout from "../components/layout";
+import withApollo from "../lib/apollo";
 
 function ProfileCard({ user }) {
   return (
@@ -18,17 +18,17 @@ function ProfileCard({ user }) {
         <p>name: {user.name}</p>
       </div>
     </>
-  )
+  );
 }
 
 function Profile() {
-  const { user, loading } = useFetchUser({ required: true })
+  const { user, loading } = useFetchUser({ required: true });
 
   return (
     <Layout user={user} loading={loading}>
       {loading ? <>Loading...</> : <ProfileCard user={user} />}
     </Layout>
-  )
+  );
 }
 
-export default withApollo(Profile)
+export default withApollo(Profile, { ssr: false });
